01c0632f6bafda3689150d0948af6ff77c1b1d7a
AcharyaHari01 <acharyahari537@gmail.com>
2025-05-08 10:12:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let sc = 33;
+
+console.log(typeof sc);
+let score = "33";
+
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33"=> 33
+// "33abc"=> NaN
+// true =>1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0=>false
+// "" => false 
+// "hari"=>true
+
+let someNumber = 33 
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
