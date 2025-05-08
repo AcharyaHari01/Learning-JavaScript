17f0f017fd7b33865c6dc75c904a10b83f405b84
AcharyaHari01 <acharyahari537@gmail.com>
2025-05-08 09:57:25 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not brower
+
+let name = "hari"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number  => 2 to power53
+// bigint 
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined =>
+// symbol =>unique 
+
+// object 
+
+console.log(typeof "hari");
+console.log(typeof null); //object
